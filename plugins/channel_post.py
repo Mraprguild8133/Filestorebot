ddf82adc987143739af8695f50e973fea4c4187b
Mraprguild8133 <mraprguild06@gmail.com>
2025-10-02 19:30:43 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,32 +1,31 @@
-import asyncio
-from pyrogram import filters, Client
+from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 
 from bot import Bot
-from config import *
-from helper_func import encode, admin
+from helper_func import admin, encode
 
-@Bot.on_message(filters.private & admin & ~filters.command(['start', 'commands','users','broadcast','batch', 'custom_batch', 'genlink','stats', 'dlt_time', 'check_dlt_time', 'dbroadcast', 'ban', 'unban', 'banlist', 'addchnl', 'delchnl', 'listchnl', 'fsub_mode', 'pbroadcast', 'add_admin', 'deladmin', 'admins', 'delreq']))
-async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Please Wait...!", quote = True)
+@Bot.on_message(filters.private & admin & ~filters.command(['start', 'help']))
+async def channel_post(client: Bot, message: Message):
     try:
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        # Forward message to channel
+        post_message = await message.copy(client.db_channel.id)
+        
+        # Generate link
+        converted_id = post_message.id * abs(client.db_channel.id)
+        string = f"get-{converted_id}"
+        base64_string = await encode(string)
+        link = f"https://t.me/{client.username}?start={base64_string}"
+
+        await message.reply(
+            f"**✅ File Stored**\n\n**Link:** {link}",
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("🔗 Share", url=f'https://telegram.me/share/url?url={link}')]
+            ])
+        )
+        
     except FloodWait as e:
-        await asyncio.sleep(e.x)
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        await asyncio.sleep(e.value)
+        await channel_post(client, message)
     except Exception as e:
-        print(e)
-        await reply_text.edit_text("Something went Wrong..!")
-        return
-    converted_id = post_message.id * abs(client.db_channel.id)
-    string = f"get-{converted_id}"
-    base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
-
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
-
-    if not DISABLE_CHANNEL_BUTTON:
-        await post_message.edit_reply_markup(reply_markup)
+        await message.reply("❌ Error storing file")
